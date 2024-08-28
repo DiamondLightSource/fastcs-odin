c88bd857b5815a0d2cc1477e9105d230b3f0a320
Gary Yendell <gary.yendell@diamond.ac.uk>
2024-08-28 12:42:44 +0000

    Handle meta and fan parameters correctly

--- src/fastcs_odin/eiger_fan.py
+++ src/fastcs_odin/eiger_fan.py
@@ -1,16 +1,14 @@
-from fastcs.attributes import AttrRW
-from fastcs.datatypes import Int, String
-
-from fastcs_odin.io.parameter_attribute_io import ParameterTreeAttributeIORef
 from fastcs_odin.odin_adapter_controller import OdinAdapterController
 
 
 class EigerFanAdapterController(OdinAdapterController):
-    """SubController for an eigerfan adapter in an odin control server."""
+    """Controller for an EigerFan adapter in an odin control server"""
 
-    acquisition_id: AttrRW = AttrRW(
-        String(), io_ref=ParameterTreeAttributeIORef("api/0.1/ef/0/config/acqid")
-    )
-    block_size: AttrRW = AttrRW(
-        Int(), io_ref=ParameterTreeAttributeIORef("api/0.1/ef/0/config/block_size")
-    )
+    def _process_parameters(self):
+        for parameter in self.parameters:
+            # Remove 0 index and status/config
+            match parameter.uri:
+                case ["0", "status" | "config", *_]:
+                    parameter.set_path(parameter.path[2:])
+                case ["0", _]:
+                    parameter.set_path(parameter.path[1:])
